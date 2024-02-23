068d7c81bc3fea96fb8ec9cf14a6886dafb7b53d
MichyPotato <96063090+MichyPotato@users.noreply.github.com>
2024-02-22 21:20:55 -0500

    Add files via upload

---
+++ DRAKO - text based adventure game/src/luo/seven/OnInventory.java
@@ -0,0 +1,14 @@
+/* Michelle Luo
+ * Cmdr. Schenk
+ * October 19th
+ * AP CSA
+ * On Inventory ENUM Class
+ */
+
+package luo.seven;
+
+public enum OnInventory {
+    carrying,
+    onGround,
+    heldByCharacter
+}
